52b117951a2902eec1b11302996e65c9a325494b
cherif <kheffacherif@yahoo.fr>
2023-02-16 18:12:14 +0100

    premier commit creation de la classe pageEvent

---
+++ src/main/java/com/example/springcloudstreamsfunctionkafka/SpringCloudStreamsFunctionKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudstreamsfunctionkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamsFunctionKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamsFunctionKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcloudstreamsfunctionkafka/SpringCloudStreamsFunctionKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudstreamsfunctionkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamsFunctionKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
